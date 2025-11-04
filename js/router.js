22a920738e84fc7b9987d728b1a82d5bb96cb019
iFruze <denisbyev4s@gmail.com>
2025-11-04 15:13:57 +0300

    change router.js

--- js/router.js
+++ js/router.js
@@ -1,4 +1,6 @@
 import { renderUsers } from './components/users.js';
+import { renderTodos } from './components/todos.js';
+import { renderPosts } from './components/posts.js';
 
 
 export function router() {
@@ -9,8 +11,8 @@ export function router() {
   renderBreadcrumbs(hash, app);
 
   if (hash === '#users') renderUsers(app);
-  else if (hash === '#users#todos') console.log('app');
-  else if (hash === '#users#posts') console.log('app');
+  else if (hash === '#users#todos') renderTodos(app);
+  else if (hash === '#users#posts') renderPosts(app);
   else if (hash === '#users#posts#comments') console.log('app');
   else app.innerHTML = '<h2>Выберите раздел</h2>';
 }
